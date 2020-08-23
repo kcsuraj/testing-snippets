a56dd81da7e3b1866f8013589aad2826f86d45aa
Suraj kc <fmi-12@Surajs-MacBook-Air.local>
2020-08-23 18:29:27 +0545

    Test submitting a form

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const globalTypes = {
+  theme: {
+    name: "Testing Snippets",
+    description: "Collection of useful test snippets",
+    defaultValue: "light",
+    toolbar: {
+      icon: "circlehollow",
+      // array of plain string values or MenuItem shape (see below)
+      items: ["light", "dark"],
+    },
+  },
+};
